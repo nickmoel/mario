a4058061b490a3296b04a5c7089a89daf7fe9b2c
Nicholas Moeller <nwmoeller75@gmail.com>
2017-07-05 18:48:18 -0500

    added Pyramid Class
    changed code for the pyramid class and moved the core logic

--- Mario.java
+++ Mario.java
@@ -3,27 +3,29 @@ package Mario;
 import java.util.*;
 
 public class Mario {
+	public static void main(String[] args) {
+		int n;
+	    String output_choice;
+	    Scanner console = new Scanner(System.in);
+
+	    do {
+	        System.out.print("How many blocks to jump between 0-23? ");
+	        n = console.nextInt();
+	    }
+	    while (n < 0 || n > 23);
 
 
-    public static void main(String[] args) {
-        int n;
-        Scanner console = new Scanner(System.in);
+	    do {
+	        System.out.print("press c for console or f for text file:");
+	        output_choice = console.next();
+	    }while (!((output_choice.equals("c")) || (output_choice.equals("f"))));
 
-        do {
-            System.out.print("How many blocks to jump between 0-23? ");
-            n = console.nextInt();
-        }
-        while (n < 0 || n > 23);
+	    Printer choice;
+	    choice = output_choice.equals("c") ? new Console() : new SaveFile();
 
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= n +1; j++) {
-                if (j >= i) {
-                    System.out.print("#");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-    }
-}
+	    Pyramid pyramid = new Pyramid();
+	    pyramid.setRows(n);
+	    choice.print(pyramid);
+
+	}
+	}
