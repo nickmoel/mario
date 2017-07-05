2580bd883ca864127583db12eed705bfd4d868ed
Nicholas Moeller <nwmoeller75@gmail.com>
2017-07-05 10:25:18 -0500

    Changes to Mario for problem set 2

--- Mario.java
+++ Mario.java
@@ -1,5 +1,6 @@
-package Mario;
-
+package Mario;/**
+ * Created by Nick on 6/21/2017.
+ */
 import java.util.*;
 
 public class Mario {
@@ -15,6 +16,16 @@ public class Mario {
         }
         while (n < 0 || n > 23);
 
+        do {
+            System.out.print("press c for console or f for text file:");
+            output_choice = console.next();
+        }while (!((output_choice.equals("c")) || (output_choice.equals("f"))));
+
+        PyramidPrinter choice;
+        choice = output_choice.equals("c") ? new PyramidScreenPrinter() : new PyramidFilePrinter();
+
+        StringBuilder sb = new StringBuilder();
+
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= n +1; j++) {
                 if (j >= i) {
@@ -23,7 +34,8 @@ public class Mario {
                     System.out.print(" ");
                 }
             }
-            System.out.println();
+            sb.append("\n");
         }
+        choice.print(sb);
     }
 }
