9b8a290068acde01656b110572e44d6b66b790f6
Nicholas Moeller <nwmoeller75@gmail.com>
2017-07-11 12:46:55 -0500

    "still having problems learning git"

--- Mario.java
+++ Mario.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package Mario;
 
 /**
@@ -29,3 +30,36 @@ public class Mario {
 
     }
    }
+=======
+package Mario;
+
+/**
+ * Created by Nick on 6/30/2017.
+ */
+import java.util.*;
+
+
+public class Mario {
+
+    public static void main(String[] args) {
+        int n;
+        String output_choice;
+        Scanner console = new Scanner(System.in);
+
+        do {
+            System.out.print("How many blocks to jump between 0-23? ");
+            n = console.nextInt();
+        }
+        while (n < 0 || n > 23);
+
+
+        do {
+            System.out.print("press c for console or f for text file:");
+            output_choice = console.next();
+        }while (!((output_choice.equals("c")) || (output_choice.equals("f"))));
+
+        PrinterFactory.createPrinter(output_choice).print(PyramidFactory.withRows(n));
+
+    }
+   }
+>>>>>>> ae0361428291f857444fefe9bfdf6f1af5d85334
