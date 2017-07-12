3814e5bb4d58e1e61d74795e2f7e453f3b76e69d
Nicholas Moeller <nwmoeller75@gmail.com>
2017-07-12 17:36:04 -0500

    Updated Mario.java
    Having problems between my local and this remote branch. Trying to figure it all out.

--- Mario.java
+++ Mario.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package Mario;
 
 /**
@@ -31,69 +29,3 @@ public class Mario {
 
     }
    }
-=======
-package Mario;
-
-/**
- * Created by Nick on 6/30/2017.
- */
-import java.util.*;
-
-
-public class Mario {
-
-    public static void main(String[] args) {
-        int n;
-        String output_choice;
-        Scanner console = new Scanner(System.in);
-
-        do {
-            System.out.print("How many blocks to jump between 0-23? ");
-            n = console.nextInt();
-        }
-        while (n < 0 || n > 23);
-
-
-        do {
-            System.out.print("press c for console or f for text file:");
-            output_choice = console.next();
-        }while (!((output_choice.equals("c")) || (output_choice.equals("f"))));
-
-        PrinterFactory.createPrinter(output_choice).print(PyramidFactory.withRows(n));
-
-    }
-   }
->>>>>>> ae0361428291f857444fefe9bfdf6f1af5d85334
-=======
-package Mario;
-
-/**
- * Created by Nick on 6/30/2017.
- */
-import java.util.*;
-
-
-public class Mario {
-
-    public static void main(String[] args) {
-        int n;
-        String output_choice;
-        Scanner console = new Scanner(System.in);
-
-        do {
-            System.out.print("How many blocks to jump between 0-23? ");
-            n = console.nextInt();
-        }
-        while (n < 0 || n > 23);
-
-
-        do {
-            System.out.print("press c for console or f for text file:");
-            output_choice = console.next();
-        }while (!((output_choice.equals("c")) || (output_choice.equals("f"))));
-
-        PrinterFactory.createPrinter(output_choice).print(PyramidFactory.withRows(n));
-
-    }
-   }
->>>>>>> ae0361428291f857444fefe9bfdf6f1af5d85334
